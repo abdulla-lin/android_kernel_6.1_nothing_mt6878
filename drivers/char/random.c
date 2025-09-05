70ff42770ffe5d4b7897563e4c97d1c5831fca18
abdulla-lin <abdullashakkeeb4@gmail.com>
2025-09-05 20:21:28 +0530

    Update random.c

--- drivers/char/random.c
+++ drivers/char/random.c
@@ -61,12 +61,6 @@
 #include <asm/io.h>
 #include <linux/random.h>
 #include <linux/ktime.h>
-static void __init chick_rng_tweak(void)
-{
-    pr_info("ChickKernel: RNG pool optimized\n");
-    prandom_seed_fast(ktime_get_ns()); // faster seeding
-}
-early_initcall(chick_rng_tweak);
 #endif
 /*********************************************************************
  *
